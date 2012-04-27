9895e4b61cc230e196e75b67bbb42cb8266c016d
Lukas Domagala <Domagala.Lukas@gmail.com>
2012-04-27 22:35:35 +0200

    now only \r \n and \r\n count as newlines, configuring it would be to much hassle

--- src/clparsec/core.clj
+++ src/clparsec/core.clj
@@ -29,17 +29,19 @@
   ALocation (location-code [this] (format "line %s" line)))
 
 (defprotocol ALineAndColumnLocation
-  (location-inc-line [location])
-  (location-inc-column [location])
-  (location-plus-column [location n]))
+  (inc-line [location])
+  (inc-column [location])
+  (plus-column [location n])
+  (plus-line [location n]))
 
 (defrecord StandardLocation [line column]
   ALocation
     (location-code [this] (format "line %s, column %s" line column))
   ALineAndColumnLocation
-    (location-inc-line [this] (assoc this :line (inc line), :column 1))
-    (location-inc-column [this] (assoc this :column (inc column)))
-    (location-plus-column [this n] (assoc this :column (+ column n))))
+    (inc-line [this] (assoc this :line (inc line), :column 1))
+    (inc-column [this] (assoc this :column (inc column)))
+    (plus-column [this n] (assoc this :column (+ column n)))
+    (plus-line [this n] (assoc this :line (+ line n))))
 
 (defn make-standard-location [line column]
   {:pre #{(integer? line) (integer? column)}}
@@ -47,9 +49,8 @@
 
 (defn standard-alter-location [character]
   (if (*newline-chars* character)
-    location-inc-line location-inc-column))
-(defn location? [obj]
-  (extends? ALocation (type obj)))
+    inc-line inc-column))
+
 (defprotocol AState
   "The protocol of FnParse states, which must
   be able to return a position."
@@ -69,7 +70,7 @@
   (read-char-or-newline [state]))
 
 (defrecord State
-  [remainder position location warnings context alter-location]
+  [remainder position location warnings context]
   AState
     (get-position [this] position)
     (get-remainder [this] remainder)
@@ -80,7 +81,7 @@
       (when-let [remainder (seq remainder)]
         (assoc this
                :remainder (next remainder), :position (inc position),
-               :location ((alter-location (first remainder)) location))))
+               :location ((standard-alter-location (first remainder)) location))))
     (skip [this c] (when (=(first remainder)c)
                          (next-state this)))
     (skip-newline [this] 
@@ -88,7 +89,7 @@
                     (if (= \newline (second remainder))
                       (assoc this 
                              :remainder (next (next remainder)), :position (+ position 2)
-                             :location ((alter-location "\r\n") location))
+                             :location ((standard-alter-location "\r\n") location))
                       (next-state this))
                     (if (*newline-chars* (first remainder))
                       (next-state this))))
@@ -98,17 +99,25 @@
                               (if (= \newline (second remainder))
                                 (list (assoc this 
                                              :remainder (next (next remainder)), :position (+ position 2)
-                                             :location ((alter-location "\r\n") location))
+                                             :location ((standard-alter-location "\r\n") location))
                                       \newline)
                                 (list (next-state this) \newline))
                               (list (next-state this) c))))
     (peep [this] (first remainder))
     (skip-one [this] (next-state this))
-    (skip-whitespace [this] (when (*whitespace* (first remainder)) 
-                              (loop [state this]
-                                  (if (*whitespace* (peep state)) ; check buggy?
-                                    (recur (next-state state))
-                                    state))))
+    (skip-whitespace [this] 
+                     (when (*whitespace* (first remainder))
+                       (loop [state this]
+	                       (let [c (peep state)]
+	                         (if (= \return c)
+	                           (if (= \newline (second (:remainder state)))
+	                             (recur (assoc state 
+	                                          :remainder (next (next (:remainder state))), :position (+ (:position state) 2)
+	                                          :location ((standard-alter-location "\r\n") (:location state))))                                   
+	                             (recur (next-state state)))
+	                           (if (*whitespace* c)
+	                             (recur (next-state state))
+	                             state))))))
     (skip-strn [this strn] 
                  (let [length (count strn)]
                    (when (< length (count remainder))
@@ -118,7 +127,7 @@
                            (recur (inc i)))
                          (assoc this :remainder (.substring remainder i)
                                 :position (+ position i)
-                                :location (location-plus-column location i)))))))
+                                :location (plus-column location i)))))))
     (read-chars-or-newlines-while [this pred1 pred normalize-n] ;todo normalize newlines
                                     (if (pred1 (first remainder))
                                       (loop[state (next-state this)
@@ -133,11 +142,9 @@
 
 (defn make-state
   "Creates a state with the given parameters."
-  [input & {:keys #{location context alter-location}
-            :or {location (make-standard-location 1 1), alter-location
-                 standard-alter-location}}]
-  {:pre #{(or (nil? location) (location? location)) (ifn? alter-location)}}
-  (State. input 0 location #{} context alter-location))
+  [input & {:keys #{location context}
+            :or {location (make-standard-location 1 1)}}]
+  (State. input 0 location #{} context))
 
 
 (defn- merge2-errors
